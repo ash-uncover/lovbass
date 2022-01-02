70c0c4a13672bbbf5dcfa1db4bb43231a8f916b6
ash-uncover <ash.uncover@gmail.com>
2022-01-02 22:58:39 +0100

    go on lovebass

--- src/components/app/sets/Sets.jsx
+++ src/components/app/sets/Sets.jsx
@@ -17,12 +17,16 @@ import {
   selectors as SetSelectors
 } from 'store/data/sets'
 
+import {
+  selectors as SongSelectors,
+} from 'store/data/songs'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faArrowLeft,
 } from '@fortawesome/free-solid-svg-icons'
 
-import Set from 'components/common/Set'
+import Song from 'components/common/Song'
 
 import './Sets.less'
 
@@ -31,7 +35,10 @@ const Sets = () => {
     <Routes>
       <Route path='/'>
         <Route index element={<SetsList />} />
-        <Route path=':setId' element={<SetsEntry />} />
+        <Route path=':setId'>
+          <Route index element={<SetsEntry />} />
+          <Route path=':songId' element={<SetsEntrySong />} />
+        </Route>
       </Route>
     </Routes>
   )
@@ -76,11 +83,41 @@ const SetsEntry = () => {
 
   return (
     <div className='sets-entry'>
-      <Link to='/sets'>
+      { setId !== 'latest' ? (
+        <Link to='/sets'>
+          <FontAwesomeIcon icon={faArrowLeft} />
+          {t('app:events.entry.back')}
+        </Link>
+      ) : null}
+      <div className='set'>
+        <h2 className='set-title'>{set.date} - {set.place} - {set.songs.length}</h2>
+        <ul>
+          {set.songs.map((song) => {
+            return (
+              <li className='set-song-entry' key={song}>
+                <Link className='set-song-link' to={`/sets/${set.date}/${song}`}>
+                  {song}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const SetsEntrySong = () => {
+  const { t } = useTranslation()
+  const { setId, songId } = useParams()
+  const song = useSelector(SongSelectors.selectSong(songId))
+  return (
+    <div className='sets-entry-song'>
+      <Link to={`/sets/${setId}`}>
         <FontAwesomeIcon icon={faArrowLeft} />
-        {t('app:events.entry.back')}
+        {t('app:events.entry.song.back')}
       </Link>
-      <Set {...set} />
+      <Song {...song} />
     </div>
   )
 }
