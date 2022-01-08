90e6330dc30f473a26dbe49ac6fbb289ca02f787
ash-uncover <ash.uncover@gmail.com>
2022-01-08 08:53:07 +0100

    try again

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,13 +4,14 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.config.common.js')
 
 const path = require('path')
+const dir_build = path.resolve(__dirname, 'docs')
 
 module.exports = merge(common, {
   mode: 'production',
 
   output: {
-    path: path.resolve(__dirname),
-    filename: '[name].bundle.js',
-    publicPath: '/',
+    path: dir_build,
+    filename: 'bundle.js',
+    publicPath: './',
   },
 })
